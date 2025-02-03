09546c4493057fcfaaf3d37c8ba255d7c0f75234
phlimadev <devphlima@gmail.com>
2025-02-03 12:59:45 -0300

    adding method of calculating statistics

--- src/main/java/com/example/itau_challenge/services/TransactionService.java
+++ src/main/java/com/example/itau_challenge/services/TransactionService.java
@@ -1,10 +1,14 @@
 package com.example.itau_challenge.services;
 
+import com.example.itau_challenge.dtos.StatisticsDTO;
 import com.example.itau_challenge.dtos.TransactionDTO;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
@@ -26,5 +30,12 @@ public class TransactionService {
 
     public void deleteAllTransactions() {
         transactionList.clear();
+    }
+
+    public StatisticsDTO calculateStatistics(Long seconds) {
+        OffsetDateTime interval = OffsetDateTime.now().minusSeconds(seconds);
+        List<TransactionDTO> filteredList = transactionList.stream().filter(transaction -> transaction.dataHora().isAfter(interval)).toList();
+        DoubleSummaryStatistics statistics = filteredList.stream().collect(Collectors.summarizingDouble(TransactionDTO::valor));
+        return new StatisticsDTO(statistics.getCount(), statistics.getSum(), statistics.getAverage(), statistics.getMin(), statistics.getMax());
     }
 }
