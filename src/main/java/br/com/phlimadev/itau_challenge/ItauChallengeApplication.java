243fb8c968e16410b98e8d401c504a9fdb3b5cb0
phlimadev <devphlima@gmail.com>
2025-05-23 10:08:40 -0300

    first commit

---
+++ src/main/java/br/com/phlimadev/itau_challenge/ItauChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.phlimadev.itau_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauChallengeApplication.class, args);
+	}
+
+}
