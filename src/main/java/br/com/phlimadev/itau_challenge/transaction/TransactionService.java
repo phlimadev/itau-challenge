0dbb6aafc9139352933760d50f58cd4926889ca7
phlimadev <devphlima@gmail.com>
2025-05-23 10:51:26 -0300

    feat: creating method to get statistics

--- src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionService.java
+++ src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionService.java
@@ -1,12 +1,16 @@
 package br.com.phlimadev.itau_challenge.transaction;
 
+import br.com.phlimadev.itau_challenge.statistics.StatisticsDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
-    List<TransactionDTO> transactions;
+    List<TransactionDTO> transactions = new ArrayList<>();
 
     public void save(TransactionDTO transaction) {
         transactions.add(transaction);
@@ -14,5 +18,10 @@ public class TransactionService {
 
     public void clear() {
         transactions.clear();
+    }
+
+    public StatisticsDTO getStatistics() {
+        DoubleSummaryStatistics statistics = transactions.stream().collect(Collectors.summarizingDouble(TransactionDTO::valor));
+        return new StatisticsDTO(statistics.getCount(), statistics.getSum(), statistics.getAverage(), statistics.getMin(), statistics.getMax());
     }
 }
