458d06c8f50df98c06f679aa4437ddecbfc85705
phlimadev <devphlima@gmail.com>
2025-05-23 10:58:45 -0300

    feat: creating StatisticsController

---
+++ src/main/java/br/com/phlimadev/itau_challenge/statistics/StatisticsController.java
@@ -0,0 +1,22 @@
+package br.com.phlimadev.itau_challenge.statistics;
+
+import br.com.phlimadev.itau_challenge.transaction.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+    private final TransactionService transactionService;
+
+    public StatisticsController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsDTO> getStatistics() {
+        return ResponseEntity.ok(transactionService.getStatistics());
+    }
+}
