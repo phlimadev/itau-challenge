9003b2b41d4cca02012fc465e5af9f850ab7c65a
phlimadev <devphlima@gmail.com>
2025-05-23 10:15:30 -0300

    feat: ensuring that the transaction MUST NOT have a negative value; the transaction MUST have a value equal to or greater than 0 (zero)

--- src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionDTO.java
+++ src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionDTO.java
@@ -2,12 +2,14 @@ package br.com.phlimadev.itau_challenge.transaction;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 public record TransactionDTO(
         @NotNull
+        @PositiveOrZero
         BigDecimal valor,
         @NotNull
         @Past
