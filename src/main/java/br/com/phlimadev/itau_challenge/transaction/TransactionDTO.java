abe69a156da1456a04ed72d5d83f95f8c27193e5
phlimadev <devphlima@gmail.com>
2025-05-23 10:11:30 -0300

    feat: ensuring that the API only receives transactions that have the value and dateTime fields filled in

--- src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionDTO.java
+++ src/main/java/br/com/phlimadev/itau_challenge/transaction/TransactionDTO.java
@@ -1,10 +1,14 @@
 package br.com.phlimadev.itau_challenge.transaction;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 public record TransactionDTO(
+        @NotNull
         BigDecimal valor,
+        @NotNull
         OffsetDateTime dataHora
 ) {
 }
